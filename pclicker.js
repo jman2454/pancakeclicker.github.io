e35684f4cb28c89c164faa559fc17224078d2bc4
jman2454 <james.meyers212@gmail.com>
2015-10-20 23:15:56 -0400

    Updated a lot, made game run more smoothly

--- pclicker.js
+++ pclicker.js
@@ -1,4 +1,3 @@
-var cookies = 0;
 var grandmas = 0;
 var farms = 0;
 var factories = 0;
@@ -10,114 +9,71 @@ var factoryprice = 500;
 var kitchenprice = 1000;
 var ihopprice = 10000;
 var pps = 0;
+var lastTime = new Date().getTime();
+var tp = 0;
 
 function clickCookie() {
-    cookies++;
+    tp++;
     el = document.getElementById("message");
-    el.innerHTML = Beautify(cookies);
+    el.innerHTML = Beautify(tp);
 }
 
 function TitleUpdate() {
-    document.title = "Pancake Clicker - " + cookies + " Pancakes";
+    document.title = "Pancake Clicker - " + tp + " Pancakes";
 }
 
 function buyGrandma() {
-    if (cookies >= gprice) {
-        cookies -= gprice
-        grandmas++
-        gprice += 7
-        pps += 1.7
+    if (tp >= gprice) {
+        tp -= gprice;
+        grandmas++;
+        gprice *= 1.2;
+        pps += 1.7;
         el = document.getElementById("grandmasale");
         el.innerHTML = grandmas;
-        el = document.getElementById("message");
-        el.innerHTML = Beautify(cookies);
-        setInterval(function () {
-            for (var i = 0; i < 1; i++) {
-                cookies += 1
-                el = document.getElementById("message");
-                el.innerHTML = Beautify(cookies);
-            }
-        }, 1000);
     } else {}
 }
 
 function buyFarm() {
-    if (cookies >= farmprice) {
-        cookies -= farmprice
-        farms++
-        farmprice += 12
-        pps += 3
+    if (tp >= farmprice) {
+        tp -= farmprice;
+        farms++;
+        farmprice *= 1.3;
+        pps += 3;
         el = document.getElementById("farmsale");
         el.innerHTML = farms;
-        el = document.getElementById("message");
-        el.innerHTML = Beautify(cookies);
-        setInterval(function () {
-            for (var i = 0; i < 3; i++) {
-                cookies += 1
-                el = document.getElementById("message");
-                el.innerHTML = Beautify(cookies);
-            }
-        }, 1000);
     } else {}
 }
 
 function buyFactory() {
-    if (cookies >= factoryprice) {
-        cookies -= factoryprice
-        factories++
-        factoryprice += 21
-        pps += 5
+    if (tp >= factoryprice) {
+        tp -= factoryprice;
+        factories++;
+        factoryprice *= 1.5;
+        pps += 5;
         el = document.getElementById("factorysale");
         el.innerHTML = factories;
-        el = document.getElementById("message");
-        el.innerHTML = Beautify(cookies);
-        setInterval(function () {
-            for (var i = 0; i < 5; i++) {
-                cookies += 1
-                el = document.getElementById("message");
-                el.innerHTML = Beautify(cookies);
-            }
-        }, 1000);
     } else {}
 }
 
 function buyKitchen() {
-    if (cookies >= kitchenprice) {
-        cookies -= kitchenprice
-        kitchens++
-        kitchenprice += 43
-        pps += 8.3
+    if (tp >= kitchenprice) {
+        tp -= kitchenprice;
+        kitchens++;
+        kitchenprice *= 1.7;
+        pps += 8.3;
         el = document.getElementById("kitchensale");
         el.innerHTML = kitchens;
-        el = document.getElementById("message");
-        el.innerHTML = Beautify(cookies);
-        setInterval(function () {
-            for (var i = 0; i < 8; i++) {
-                cookies += 1
-                el = document.getElementById("message");
-                el.innerHTML = Beautify(cookies);
-            }
-        }, 1000);
     } else {}
 }
 
 function buyIhop() {
-    if (cookies >= ihopprice) {
-        cookies -= ihopprice
+    if (tp >= ihopprice) {
+        tp -= ihopprice;
         ihops++;
-        ihopprice += 67;
+        ihopprice *= 2;
         pps += 100;
         el = document.getElementById("ihopsale");
         el.innerHTML = ihops;
-        el = document.getElementById("message");
-        el.innerHTML = Beautify(cookies);
-        setInterval(function () {
-            for (var i = 0; i < 100; i++) {
-                cookies += 1
-                el = document.getElementById("message");
-                el.innerHTML = Beautify(cookies);
-            }
-        }, 1000);
     } else {}
 }
 
@@ -127,20 +83,21 @@ function Beautify(what) //turns 9999999 into 9,999,999
     what = Math.floor(what);
     what = (what + '').split('').reverse();
     for (var i in what) {
-        if (i % 3 == 0 && i > 0) str = ',' + str;
+        if (i % 3 === 0 && i > 0) str = ',' + str;
         str = what[i] + str;
     }
     return str;
 }
-function show() { 
-        if(document.getElementById('shop').style.display=='none') { 
-            document.getElementById('shop').style.display='block'; 
-        } 
-        return false;
-    } 
-    function hide() { 
-        if(document.getElementById('shop').style.display=='block') { 
-            document.getElementById('shop').style.display='none'; 
-        } 
-        return false;
-    }   
+
+setInterval(function () {
+  var currentTime = new Date().getTime();
+  var elapsedTime = currentTime - lastTime;
+
+  lastTime = currentTime;
+  
+  tp += (pps * (elapsedTime / 1000)); // elapsedTime is in milliseconds, divide by 1000 to get fractional seconds
+  
+  document.getElementById('message').innerText = Math.floor(tp);
+  
+  
+}, 10);
